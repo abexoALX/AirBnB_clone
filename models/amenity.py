c5a558fcbbc8700c2b515a9247e6eebc43257f80
henok1992 <henokgithub@gmail.com>
2022-10-30 14:20:27 -0700

    menity

--- models/amenity.py
+++ models/amenity.py
@@ -2,7 +2,6 @@
 """ Amenity class, inherits from BaseModel """
 from models.base_model import BaseModel
 
-
 class Amenity(BaseModel):
     """ Amenity class, inherits from BaseModel """
     name = ""
